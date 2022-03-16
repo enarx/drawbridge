77abeac7062c222827a7ef9df37080da316b5b23
Nathaniel McCallum <nathaniel@profian.com>
2022-03-16 17:51:05 -0400

    chore: rework folder layout slightly
    1. I want to group the reusable library crates under a single
    subdirectory. This groups them coherently.
    
    2. I don't want a separate drawbridge-bin. The top-level crate should be
    the main binary. If we find we really want a reusable composite service,
    we can rearrange at that time.
    
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use axum::routing::any_service;
 use axum::Router;
 
+use drawbridge_tags::new as tags;
+use drawbridge_tree::new as tree;
+
 pub fn new() -> Router {
     Router::new()
-        .nest("/_tree", any_service(tree::new()))
-        .nest("/_tags", any_service(tags::new()))
+        .nest("/_tree", any_service(tree()))
+        .nest("/_tags", any_service(tags()))
     // TODO: Add auth
     // TODO: Figure out how to handle namespacing
 }
