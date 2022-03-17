45fa6dceb6c6232f2c910416db992ba5e774fa70
Roman Volosatovs <roman@profian.com>
2022-03-17 17:35:46 +0100

    feat: add fallbacks to unimplemented sub-services
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/tags/src/lib.rs
+++ crates/tags/src/lib.rs
@@ -1,5 +1,9 @@
+use axum::handler::Handler;
+use axum::http::StatusCode;
 use axum::Router;
 
 pub fn new() -> Router {
-    Router::new()
+    Router::new().fallback(
+        (|| async { (StatusCode::NOT_IMPLEMENTED, "Tag handling not implemented") }).into_service(),
+    )
 }
--- crates/tree/src/lib.rs
+++ crates/tree/src/lib.rs
@@ -1,5 +1,10 @@
+use axum::handler::Handler;
+use axum::http::StatusCode;
 use axum::Router;
 
 pub fn new() -> Router {
-    Router::new()
+    Router::new().fallback(
+        (|| async { (StatusCode::NOT_IMPLEMENTED, "Tree handling not implemented") })
+            .into_service(),
+    )
 }
