d1bd28a34d305e7db573fd563b805c50816cb3e3
Roman Volosatovs <roman@profian.com>
2022-04-19 15:38:05 +0200

    fix(tags): reenable tag service tests
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/tags/src/lib.rs
+++ crates/tags/src/lib.rs
@@ -101,219 +101,220 @@ pub fn app() -> Router {
     Router::new()
 }
 
-//// TODO: Reenable
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    use std::collections::{BTreeMap, HashMap};
-//
-//    use axum::http::{Request, Method};
-//    use drawbridge_jose::b64::Json;
-//    use drawbridge_jose::jws::{Flattened, General, Jws, Signature};
-//    use serde_json::json;
-//
-//    #[tokio::test]
-//    async fn name_from_request() {
-//        fn new_request(path: impl AsRef<str>) -> Request {
-//            let mut req = Request::new(Method::Put, "https://example.com/");
-//            req.url_mut().set_path(path.as_ref());
-//            req
-//        }
-//
-//        for path in ["/", "//", "/\\/", "//test", "/=/", "/ы", "/?"] {
-//            assert!(
-//                Name::from_request(&mut new_request(path)).await.is_err(),
-//                "path '{}' should fail",
-//                path
-//            );
-//        }
-//
-//        for (path, expected, rest) in [
-//            ("/1.2.3/", "1.2.3", "/"),
-//            ("/v1.2.3/foo/bar", "v1.2.3", "/foo/bar"),
-//            ("/v1.2.3-rc1", "v1.2.3-rc1", "/"),
-//            ("/test", "test", "/"),
-//        ] {
-//            let mut req = new_request(path);
-//            assert_eq!(
-//                Name::from_request(&mut req).await.unwrap(),
-//                Name(expected.into()),
-//                "path '{}' should pass",
-//                path
-//            );
-//            assert_eq!(req.url().path(), rest);
-//        }
-//    }
-//
-//    #[tokio::test]
-//    async fn tag_from_request() {
-//        async fn from_request(
-//            content_type: Option<&str>,
-//            body: impl Into<Body>,
-//        ) -> Result<Tag> {
-//            let mut req = Request::new(Method::Put, "https://example.com/");
-//            if let Some(content_type) = content_type {
-//                req.set_content_type(content_type.into());
-//            }
-//            req.set_body(body);
-//            Tag::from_request(&mut req).await
-//        }
-//
-//        assert!(from_request(None, "").await.is_err());
-//
-//        const ALGORITHM: &str = "sha-256";
-//        const HASH: &str = "4REjxQ4yrqUVicfSKYNO/cF9zNj5ANbzgDZt3/h3Qxo=";
-//
-//        assert!(from_request(Some(Entry::TYPE), "").await.is_err());
-//        assert!(from_request(Some(Entry::TYPE), "}{").await.is_err());
-//        assert!(from_request(Some(Entry::TYPE), "test").await.is_err());
-//        assert!(from_request(Some(Entry::TYPE), json!({})).await.is_err());
-//
-//        assert!(from_request(
-//            Some(Entry::TYPE),
-//            json!({
-//                "foo": "bar",
-//            }),
-//        )
-//        .await
-//        .is_err());
-//
-//        assert_eq!(
-//            from_request(
-//                Some(Entry::TYPE),
-//                json!({
-//                    "digest": {
-//                        ALGORITHM: HASH,
-//                    },
-//                }),
-//            )
-//            .await
-//            .unwrap(),
-//            Tag::Unsigned(Entry {
-//                digest: format!("{}=:{}:", ALGORITHM, HASH).parse().unwrap(),
-//                custom: Default::default(),
-//            }),
-//        );
-//
-//        assert_eq!(
-//            from_request(
-//                Some(Entry::TYPE),
-//                json!({
-//                    "digest": {
-//                        ALGORITHM: HASH,
-//                    },
-//                    "foo": "bar",
-//                }),
-//            )
-//            .await
-//            .unwrap(),
-//            Tag::Unsigned(Entry {
-//                digest: format!("{}=:{}:", ALGORITHM, HASH).parse().unwrap(),
-//                custom: {
-//                    let mut custom = HashMap::new();
-//                    custom.insert("foo".into(), json!("bar"));
-//                    custom
-//                },
-//            }),
-//        );
-//
-//        assert!(from_request(Some(Jws::TYPE), "").await.is_err());
-//        assert!(from_request(Some(Jws::TYPE), "}{").await.is_err());
-//        assert!(from_request(Some(Jws::TYPE), "test").await.is_err());
-//        assert!(from_request(Some(Jws::TYPE), json!({})).await.is_err());
-//        assert!(from_request(
-//            Some(Jws::TYPE),
-//            json!({
-//                "foo": "bar",
-//            }),
-//        )
-//        .await
-//        .is_err());
-//
-//        const KID: &str = "e9bc097a-ce51-4036-9562-d2ade882db0d";
-//        const PAYLOAD: &str = "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ";
-//        const PROTECTED: &str = "eyJhbGciOiJFUzI1NiJ9";
-//        const SIGNATURE: &str = "DtEhU3ljbEg8L38VWAfUAqOyKAM6-Xx-F4GawxaepmXFCgfTjDxw5djxLa8ISlSApmWQxfKTUJqPP3-Kg6NU1Q";
-//
-//        let protected = || {
-//            let mut protected = BTreeMap::new();
-//            protected.insert("alg".into(), "ES256".into());
-//            Some(Json(protected))
-//        };
-//
-//        assert_eq!(
-//            from_request(
-//                Some(Jws::TYPE),
-//                json!({
-//                    "header": {
-//                        "kid": KID,
-//                    },
-//                    "payload": PAYLOAD,
-//                    "protected": PROTECTED,
-//                    "signature": SIGNATURE,
-//                })
-//            )
-//            .await
-//            .unwrap(),
-//            Tag::Signed(Jws::Flattened(Flattened {
-//                payload: PAYLOAD.parse().unwrap(),
-//                signature: Signature {
-//                    header: {
-//                        let mut header = BTreeMap::new();
-//                        header.insert("kid".into(), KID.into());
-//                        Some(header)
-//                    },
-//                    protected: protected(),
-//                    signature: SIGNATURE.parse().unwrap(),
-//                },
-//            })),
-//        );
-//
-//        assert_eq!(
-//            from_request(
-//                Some(Jws::TYPE),
-//                json!({
-//                    "payload": PAYLOAD,
-//                    "protected": PROTECTED,
-//                    "signature": SIGNATURE,
-//                })
-//            )
-//            .await
-//            .unwrap(),
-//            Tag::Signed(Jws::Flattened(Flattened {
-//                payload: PAYLOAD.parse().unwrap(),
-//                signature: Signature {
-//                    header: None,
-//                    protected: protected(),
-//                    signature: SIGNATURE.parse().unwrap(),
-//                },
-//            })),
-//        );
-//
-//        assert_eq!(
-//            from_request(
-//                Some(Jws::TYPE),
-//                json!({
-//                    "payload": PAYLOAD,
-//                    "signatures": [
-//                        {
-//                            "protected": PROTECTED,
-//                            "signature": SIGNATURE,
-//                        }
-//                    ]
-//                })
-//            )
-//            .await
-//            .unwrap(),
-//            Tag::Signed(Jws::General(General {
-//                payload: PAYLOAD.parse().unwrap(),
-//                signatures: vec![Signature {
-//                    header: None,
-//                    protected: protected(),
-//                    signature: SIGNATURE.parse().unwrap(),
-//                }],
-//            })),
-//        );
-//    }
-//}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::collections::{BTreeMap, HashMap};
+
+    use axum::body::Body;
+    use drawbridge_jose::b64::Json;
+    use drawbridge_jose::jws::{Flattened, General, Jws, Signature};
+
+    use axum::http::Request;
+    use serde_json::json;
+
+    #[tokio::test]
+    async fn name_from_request() {
+        fn new_request(path: impl AsRef<str>) -> RequestParts<()> {
+            RequestParts::new(Request::builder().uri(path.as_ref()).body(()).unwrap())
+        }
+
+        for path in ["/", "//", "/\\/", "//test", "/=/", "/?"] {
+            assert!(
+                Name::from_request(&mut new_request(path)).await.is_err(),
+                "path '{}' should fail",
+                path
+            );
+        }
+
+        for (path, expected, rest) in [
+            ("/1.2.3/", "1.2.3", "/"),
+            ("/v1.2.3/foo/bar", "v1.2.3", "/foo/bar"),
+            ("/v1.2.3-rc1", "v1.2.3-rc1", "/"),
+            ("/test", "test", "/"),
+        ] {
+            let mut req = new_request(path);
+            assert_eq!(
+                Name::from_request(&mut req).await.unwrap(),
+                Name(expected.into()),
+                "path '{}' should pass",
+                path
+            );
+            assert_eq!(req.uri().path(), rest);
+        }
+    }
+
+    #[tokio::test]
+    async fn tag_from_request() {
+        async fn from_request(
+            content_type: Option<&str>,
+            body: impl ToString,
+        ) -> Result<Tag, <Tag as FromRequest<Body>>::Rejection> {
+            let mut req = Request::builder().uri("https://example.com/").method("PUT");
+            if let Some(content_type) = content_type {
+                req = req.header("Content-Type", content_type)
+            }
+            Tag::from_request(&mut RequestParts::new(
+                req.body(Body::from(body.to_string())).unwrap(),
+            ))
+            .await
+        }
+
+        assert!(from_request(None, "").await.is_err());
+
+        const ALGORITHM: &str = "sha-256";
+        const HASH: &str = "4REjxQ4yrqUVicfSKYNO/cF9zNj5ANbzgDZt3/h3Qxo=";
+
+        assert!(from_request(Some(Entry::TYPE), "").await.is_err());
+        assert!(from_request(Some(Entry::TYPE), "}{").await.is_err());
+        assert!(from_request(Some(Entry::TYPE), "test").await.is_err());
+        assert!(from_request(Some(Entry::TYPE), json!({})).await.is_err());
+
+        assert!(from_request(
+            Some(Entry::TYPE),
+            json!({
+                "foo": "bar",
+            }),
+        )
+        .await
+        .is_err());
+
+        assert_eq!(
+            from_request(
+                Some(Entry::TYPE),
+                json!({
+                    "digest": {
+                        ALGORITHM: HASH,
+                    },
+                }),
+            )
+            .await
+            .unwrap(),
+            Tag::Unsigned(Entry {
+                digest: format!("{}=:{}:", ALGORITHM, HASH).parse().unwrap(),
+                custom: Default::default(),
+            }),
+        );
+
+        assert_eq!(
+            from_request(
+                Some(Entry::TYPE),
+                json!({
+                    "digest": {
+                        ALGORITHM: HASH,
+                    },
+                    "foo": "bar",
+                }),
+            )
+            .await
+            .unwrap(),
+            Tag::Unsigned(Entry {
+                digest: format!("{}=:{}:", ALGORITHM, HASH).parse().unwrap(),
+                custom: {
+                    let mut custom = HashMap::new();
+                    custom.insert("foo".into(), json!("bar"));
+                    custom
+                },
+            }),
+        );
+
+        assert!(from_request(Some(Jws::TYPE), "").await.is_err());
+        assert!(from_request(Some(Jws::TYPE), "}{").await.is_err());
+        assert!(from_request(Some(Jws::TYPE), "test").await.is_err());
+        assert!(from_request(Some(Jws::TYPE), json!({})).await.is_err());
+        assert!(from_request(
+            Some(Jws::TYPE),
+            json!({
+                "foo": "bar",
+            }),
+        )
+        .await
+        .is_err());
+
+        const KID: &str = "e9bc097a-ce51-4036-9562-d2ade882db0d";
+        const PAYLOAD: &str = "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ";
+        const PROTECTED: &str = "eyJhbGciOiJFUzI1NiJ9";
+        const SIGNATURE: &str = "DtEhU3ljbEg8L38VWAfUAqOyKAM6-Xx-F4GawxaepmXFCgfTjDxw5djxLa8ISlSApmWQxfKTUJqPP3-Kg6NU1Q";
+
+        let protected = || {
+            let mut protected = BTreeMap::new();
+            protected.insert("alg".into(), "ES256".into());
+            Some(Json(protected))
+        };
+
+        assert_eq!(
+            from_request(
+                Some(Jws::TYPE),
+                json!({
+                    "header": {
+                        "kid": KID,
+                    },
+                    "payload": PAYLOAD,
+                    "protected": PROTECTED,
+                    "signature": SIGNATURE,
+                })
+            )
+            .await
+            .unwrap(),
+            Tag::Signed(Jws::Flattened(Flattened {
+                payload: PAYLOAD.parse().unwrap(),
+                signature: Signature {
+                    header: {
+                        let mut header = BTreeMap::new();
+                        header.insert("kid".into(), KID.into());
+                        Some(header)
+                    },
+                    protected: protected(),
+                    signature: SIGNATURE.parse().unwrap(),
+                },
+            })),
+        );
+
+        assert_eq!(
+            from_request(
+                Some(Jws::TYPE),
+                json!({
+                    "payload": PAYLOAD,
+                    "protected": PROTECTED,
+                    "signature": SIGNATURE,
+                })
+            )
+            .await
+            .unwrap(),
+            Tag::Signed(Jws::Flattened(Flattened {
+                payload: PAYLOAD.parse().unwrap(),
+                signature: Signature {
+                    header: None,
+                    protected: protected(),
+                    signature: SIGNATURE.parse().unwrap(),
+                },
+            })),
+        );
+
+        assert_eq!(
+            from_request(
+                Some(Jws::TYPE),
+                json!({
+                    "payload": PAYLOAD,
+                    "signatures": [
+                        {
+                            "protected": PROTECTED,
+                            "signature": SIGNATURE,
+                        }
+                    ]
+                })
+            )
+            .await
+            .unwrap(),
+            Tag::Signed(Jws::General(General {
+                payload: PAYLOAD.parse().unwrap(),
+                signatures: vec![Signature {
+                    header: None,
+                    protected: protected(),
+                    signature: SIGNATURE.parse().unwrap(),
+                }],
+            })),
+        );
+    }
+}
