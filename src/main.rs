a284fc410fcc547b6e6cb906d716e8ca3e97b193
Roman Volosatovs <roman@profian.com>
2022-04-21 12:14:31 +0200

    feat: add fallback to top-level app
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use drawbridge_repo as repo;
 
+use axum::handler::Handler;
+use axum::http::{StatusCode, Uri};
 use axum::Server;
 
 #[tokio::main]
 async fn main() {
-    let app = repo::app();
+    let app = repo::app().fallback(
+        (|uri: Uri| async move { (StatusCode::NOT_FOUND, format!("Route {} not found", uri)) })
+            .into_service(),
+    );
 
     Server::bind(&SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 8080))
         .serve(app.into_make_service())
