f195bee5ae8c2f6bcbae5d96b69cfb1a80bc524e
Roman Volosatovs <roman@profian.com>
2022-04-29 13:41:23 +0200

    feat(client): add client library crate
    Signed-off-by: Roman Volosatovs <roman@profian.com>

---
+++ crates/client/src/repo.rs
@@ -0,0 +1,71 @@
+// SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
+// SPDX-License-Identifier: Apache-2.0
+
+use super::{Client, Tag};
+
+use std::error::Error;
+
+use drawbridge_type::{RepositoryConfig, RepositoryName, TagName};
+
+use reqwest::StatusCode;
+
+pub struct Repository<'a> {
+    pub(crate) client: &'a Client,
+    pub(crate) name: &'a RepositoryName,
+}
+
+impl Repository<'_> {
+    pub fn tag<'a>(&'a self, name: &'a TagName) -> Tag<'_> {
+        Tag { repo: self, name }
+    }
+
+    pub fn tags(&self) -> Result<Vec<TagName>, Box<dyn Error>> {
+        let res = self
+            .client
+            .inner
+            .get(self.client.url.join(&format!("{}/_tag", self.name))?)
+            .send()?
+            .error_for_status()?;
+        // TODO: Verify Content-Digest
+        // TODO: Verify Content-Length
+        // https://github.com/profianinc/drawbridge/issues/103
+        match res.status() {
+            StatusCode::OK => res.json().map_err(Into::into),
+            _ => Err("unexpected status code")?,
+        }
+    }
+
+    pub fn create(&self, conf: &RepositoryConfig) -> Result<bool, Box<dyn Error>> {
+        let res = self
+            .client
+            .inner
+            .put(self.client.url.join(&self.name.to_string())?)
+            // TODO: Calculate and set Content-Digest
+            // TODO: Verify if Content-Length header is set
+            // https://github.com/profianinc/drawbridge/issues/102
+            .json(conf)
+            .send()?
+            .error_for_status()?;
+        match res.status() {
+            StatusCode::CREATED => Ok(true),
+            StatusCode::OK => Ok(false),
+            _ => Err("unexpected status code")?,
+        }
+    }
+
+    pub fn get(&self) -> Result<RepositoryConfig, Box<dyn Error>> {
+        let res = self
+            .client
+            .inner
+            .get(self.client.url.join(&self.name.to_string())?)
+            .send()?
+            .error_for_status()?;
+        // TODO: Verify Content-Digest
+        // TODO: Verify Content-Length
+        // https://github.com/profianinc/drawbridge/issues/103
+        match res.status() {
+            StatusCode::OK => res.json().map_err(Into::into),
+            _ => Err("unexpected status code")?,
+        }
+    }
+}
---
+++ crates/client/src/tree.rs
@@ -0,0 +1,150 @@
+// SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
+// SPDX-License-Identifier: Apache-2.0
+
+use super::Tag;
+
+use std::error::Error;
+use std::io::Write;
+
+use drawbridge_type::{Meta, TreePath};
+
+use bytes::Bytes;
+use mime::Mime;
+use reqwest::blocking::{Body, Response};
+use reqwest::header::CONTENT_TYPE;
+use reqwest::StatusCode;
+
+pub struct Node<'a> {
+    pub(crate) tag: &'a Tag<'a>,
+    pub(crate) path: &'a TreePath,
+}
+
+impl Node<'_> {
+    pub fn create(&self, mime: Mime, body: impl Into<Body>) -> Result<bool, Box<dyn Error>> {
+        let res = self
+            .tag
+            .repo
+            .client
+            .inner
+            .put(self.tag.repo.client.url.join(&format!(
+                "{}/_tag/{}/tree{}",
+                self.tag.repo.name, self.tag.name, self.path,
+            ))?)
+            .header(CONTENT_TYPE, mime.to_string())
+            // TODO: Calculate and set Content-Digest
+            // TODO: Verify if Content-Length header is set
+            // https://github.com/profianinc/drawbridge/issues/102
+            .body(body)
+            .send()?
+            .error_for_status()?;
+        match res.status() {
+            StatusCode::CREATED => Ok(true),
+            StatusCode::OK => Ok(false),
+            _ => Err("unexpected status code")?,
+        }
+    }
+
+    fn get(&self) -> Result<(Meta, Response), Box<dyn Error>> {
+        let res = self
+            .tag
+            .repo
+            .client
+            .inner
+            .get(self.tag.repo.client.url.join(&format!(
+                "{}/_tag/{}/tree{}",
+                self.tag.repo.name, self.tag.name, self.path,
+            ))?)
+            .send()?
+            .error_for_status()?;
+        let (hash, mime) = {
+            let hdr = res.headers();
+            (
+                // TODO: figure out why CONTENT_DIGEST does not work
+                // TODO: fix parsing of Content-Digest header
+                //hdr.get("content-digest")
+                //    .ok_or("missing Content-Digest header")?
+                //    .to_str()?
+                //    .parse()
+                //    .map_err(|e| format!("failed to parse Content-Digest value: {}", e))?,
+                // https://github.com/profianinc/drawbridge/issues/103
+                Default::default(),
+                hdr.get(CONTENT_TYPE)
+                    .ok_or("missing Content-Type header")?
+                    .to_str()?
+                    .parse()?,
+            )
+        };
+        let size = res
+            .content_length()
+            .ok_or("missing Content-Length header")?;
+        match res.status() {
+            StatusCode::OK => Ok((Meta { hash, size, mime }, res)),
+            _ => Err("unexpected status code")?,
+        }
+    }
+
+    pub fn get_to(&self, dst: &mut impl Write) -> Result<(u64, Mime), Box<dyn Error>> {
+        let (
+            Meta {
+                // TODO: Validate digest
+                // https://github.com/profianinc/drawbridge/issues/103
+                hash: _,
+                size,
+                mime,
+            },
+            mut res,
+        ) = self.get()?;
+        let n = res.copy_to(dst)?;
+        if n != size {
+            Err(format!(
+                "invalid amount of bytes read, expected {}, read {}",
+                size, n,
+            ))?
+        }
+        Ok((size, mime))
+    }
+
+    pub fn get_bytes(&self) -> Result<(Bytes, Mime), Box<dyn Error>> {
+        let (
+            Meta {
+                // TODO: Validate digest
+                // https://github.com/profianinc/drawbridge/issues/103
+                hash: _,
+                size,
+                mime,
+            },
+            res,
+        ) = self.get()?;
+        let b = res.bytes()?;
+        if b.len() as u64 != size {
+            Err(format!(
+                "invalid amount of bytes read, expected {}, read {}",
+                size,
+                b.len(),
+            ))?
+        }
+        Ok((b, mime))
+    }
+
+    pub fn get_text(&self) -> Result<(String, Mime), Box<dyn Error>> {
+        let (
+            Meta {
+                // TODO: Validate digest
+                // https://github.com/profianinc/drawbridge/issues/103
+                hash: _,
+                size,
+                mime,
+            },
+            res,
+        ) = self.get()?;
+        let s = res.text()?;
+        if s.len() as u64 != size {
+            Err(format!(
+                "invalid amount of bytes read, expected {}, read {}",
+                size,
+                s.len(),
+            ))?
+        }
+        Ok((s, mime))
+    }
+}
