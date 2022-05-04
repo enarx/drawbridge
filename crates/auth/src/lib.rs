d5af247203ea25b85d306809ccb8cdedb8a916f0
Roman Volosatovs <roman@profian.com>
2022-05-04 12:59:24 +0200

    refactor(auth): migrate to `ureq`
    Remove `Error` module and enum
    
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/auth/src/lib.rs
+++ crates/auth/src/lib.rs
@@ -2,13 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod builder;
-mod error;
 mod providers;
 mod redirect;
 mod session;
 
 pub use builder::Builder;
-pub use error::Error;
 pub use providers::Provider;
 pub use redirect::AuthRedirectRoot;
 pub use session::{Session, COOKIE_NAME};
