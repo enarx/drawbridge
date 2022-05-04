6139f49d0454eecd11ce7d4a71d1646b7a6d4b93
Roman Volosatovs <roman@profian.com>
2022-05-04 12:05:12 +0200

    refactor(auth): make `Builder` methods pure
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/auth/src/builder.rs
+++ crates/auth/src/builder.rs
@@ -22,15 +22,19 @@ impl Builder {
     }
 
     /// The host the server will run on (used for redirects).
-    pub fn host(mut self, host: String) -> Self {
-        self.host = Some(host);
-        self
+    pub fn host(self, host: String) -> Self {
+        Self {
+            host: Some(host),
+            ..self
+        }
     }
 
     /// The github client id and client secret to use.
-    pub fn github(mut self, client_id: String, client_secret: String) -> Self {
-        self.github = Some((client_id, client_secret));
-        self
+    pub fn github(self, client_id: String, client_secret: String) -> Self {
+        Self {
+            github: Some((client_id, client_secret)),
+            ..self
+        }
     }
 
     /// Builds the application and returns Drawbridge instance as a [tower::MakeService].
