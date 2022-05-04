565934c071436edfaf3eea4d19129827b565a39d
Roman Volosatovs <roman@profian.com>
2022-05-04 12:43:19 +0200

    refactor(auth): introduce `session::{En,De}cryptError`
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/auth/src/session.rs
+++ crates/auth/src/session.rs
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::error::Error;
 use crate::providers::{github, Provider};
 use crate::redirect::{AuthRedirect, AuthRedirectRoot};
 
@@ -19,6 +18,50 @@ use serde::{Deserialize, Serialize};
 
 pub const COOKIE_NAME: &str = "SESSION";
 
+#[derive(Debug)]
+pub enum EncryptError {
+    Encrypt(rsa::errors::Error),
+    Serialize(bincode::Error),
+}
+
+impl fmt::Display for EncryptError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "session {}",
+            match self {
+                EncryptError::Encrypt(e) => format!("encryption error: {}", e),
+                EncryptError::Serialize(e) => format!("serialization error: {}", e),
+            }
+        )
+    }
+}
+
+impl std::error::Error for EncryptError {}
+
+#[derive(Debug)]
+pub enum DecryptError {
+    Decode(base64::DecodeError),
+    Decrypt(rsa::errors::Error),
+    Deserialize(bincode::Error),
+}
+
+impl fmt::Display for DecryptError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "session {}",
+            match self {
+                DecryptError::Decode(e) => format!("decode error: {}", e),
+                DecryptError::Decrypt(e) => format!("decryption error: {}", e),
+                DecryptError::Deserialize(e) => format!("deserialization error: {}", e),
+            }
+        )
+    }
+}
+
+impl std::error::Error for DecryptError {}
+
 // TODO: consider protections against potential replay attacks: https://github.com/profianinc/drawbridge/issues/112
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Session {
@@ -32,25 +75,25 @@ impl Session {
         Self { provider, token }
     }
 
-    /// Decrypt an untrusted user token to guarantee it was not modified by the user.
-    pub fn from_encrypted_str(string: &str, key: &RsaPrivateKey) -> Result<Self, Error> {
-        let bytes = base64::decode(string).map_err(Error::TokenDecode)?;
-        let bytes = key
-            .decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &bytes)
-            .map_err(Error::TokenDecrypt)?;
-        bincode::deserialize(&bytes).map_err(Error::TokenDeserialize)
-    }
-
     /// Encrypt the session so that it can be securely stored by the user.
-    pub fn encrypt(&self, key: &RsaPrivateKey) -> Result<String, Error> {
-        let bytes = bincode::serialize(self).map_err(Error::TokenSerialize)?;
+    pub fn encrypt(&self, key: &RsaPrivateKey) -> Result<String, EncryptError> {
+        let bytes = bincode::serialize(self).map_err(EncryptError::Serialize)?;
         key.encrypt(
             &mut OsRng,
             PaddingScheme::new_pkcs1v15_encrypt(),
             &bytes[..],
         )
-        .map_err(Error::TokenEncrypt)
+        .map_err(EncryptError::Encrypt)
         .map(base64::encode)
+    }
+
+    /// Decrypt an untrusted user token to guarantee it was not modified by the user.
+    pub fn decrypt(string: &str, key: &RsaPrivateKey) -> Result<Self, DecryptError> {
+        let bytes = base64::decode(string).map_err(DecryptError::Decode)?;
+        let bytes = key
+            .decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &bytes)
+            .map_err(DecryptError::Decrypt)?;
+        bincode::deserialize(&bytes).map_err(DecryptError::Deserialize)
     }
 }
 
@@ -88,8 +131,7 @@ where
         let session_data = cookies
             .get(COOKIE_NAME)
             .ok_or_else(|| redirect.no_error())?;
-        let session =
-            Session::from_encrypted_str(session_data, &key.0).map_err(|_| redirect.no_error())?;
+        let session = Session::decrypt(session_data, &key.0).map_err(|_| redirect.no_error())?;
 
         match session.provider {
             Provider::GitHub => github::validate(&session)
@@ -128,7 +170,7 @@ mod tests {
 
         assert_eq!(
             serde_json::to_string(
-                &Session::from_encrypted_str(&session.encrypt(&key).unwrap(), &key).unwrap()
+                &Session::decrypt(&session.encrypt(&key).unwrap(), &key).unwrap()
             )
             .unwrap(),
             serde_json::to_string(&session).unwrap()
