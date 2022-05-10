a8cf8a0d2a3d40f694ca4b0fe8206e7289fc5d35
Roman Volosatovs <roman@profian.com>
2022-05-11 01:31:36 +0200

    feat: add `digest::Algorithms::read`
    Add a utility method to compute `ContentDigest` from a reader
    
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/type/src/digest/algorithms.rs
+++ crates/type/src/digest/algorithms.rs
@@ -1,11 +1,12 @@
 // SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
 // SPDX-License-Identifier: Apache-2.0
 
-use super::{Algorithm, Reader, Writer};
+use super::{Algorithm, ContentDigest, Reader, Writer};
 
 use std::collections::BTreeSet;
 use std::ops::{Deref, DerefMut};
 
+use futures::io::{self, copy, sink, AsyncRead};
 use serde::{Deserialize, Serialize};
 
 /// A set of hashing algorithms
@@ -52,5 +53,27 @@ impl Algorithms {
     /// Creates a writer instance
     pub fn writer<T>(&self, writer: T) -> Writer<T> {
         Writer::new(writer, self.iter().cloned())
+    }
+
+    /// Calculates digest from a reader
+    pub async fn read(&self, reader: (impl AsyncRead + Unpin)) -> io::Result<ContentDigest> {
+        let mut r = self.reader(reader);
+        copy(&mut r, &mut sink()).await?;
+        Ok(r.digests())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn digest() {
+        assert_eq!(
+            Algorithms::default().read(&b"foo"[..]).await.unwrap(),
+            "sha-224=:CAj2TmDViXn8tnbJbsk4Jw3qQkRa7vzTpOb42w==:,sha-256=:LCa0a2j/xo/5m0U8HTBBNBNCLXBkg7+g+YpeiGJm564=:,sha-384=:mMEf/f3VQGdrGhN8saIrKnA1DJpEFx1rEYDGvly7LuP3nVMsih3Z7y6OCOdSo7q7=:,sha-512=:9/u6bgY2+JDlb7vzKD5STG+jIErimDgtYkdB0NxmODJuKCxBvl5CVNiCB3LFUYosWowMf37aGVlKfrU5RT4e1w==:"
+                .parse::<ContentDigest>()
+                .unwrap(),
+        );
     }
 }
