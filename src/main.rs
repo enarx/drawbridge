76f356da2a82712435ca9256ca94a1fc8545ceba
Roman Volosatovs <roman@profian.com>
2022-06-08 19:25:47 +0200

    feat: take path to the store as the only argument
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 
+use std::env::args;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use drawbridge_app as app;
@@ -10,7 +11,11 @@ use hyper::Server;
 #[tokio::main]
 async fn main() {
     Server::bind(&SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 8080))
-        .serve(app::Builder::new("drawbridge-data").build().unwrap())
+        .serve(
+            app::Builder::new(args().nth(1).expect("Store path must be specified"))
+                .build()
+                .unwrap(),
+        )
         .await
         .unwrap();
 }
