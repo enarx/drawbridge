d14788d0864fc1aece408b9667908b20a3ba9404
Roman Volosatovs <roman@profian.com>
2022-06-08 18:51:24 +0200

    test: set and validate `Content-Digest`
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/app/tests/helpers/tag.rs
+++ crates/app/tests/helpers/tag.rs
@@ -1,8 +1,12 @@
 // SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 
+use super::parse_header;
+
+use drawbridge_type::digest::{Algorithms, ContentDigest};
 use drawbridge_type::{RepositoryName, TagEntry, TagName, TreeEntry};
 
+use mime::Mime;
 use reqwest::header::CONTENT_TYPE;
 use reqwest::StatusCode;
 
@@ -13,7 +17,7 @@ pub async fn create(
     name: &TagName,
     tag: TagEntry,
 ) {
-    let url = format!("{}/{}/_tag/{}", addr, repo, name);
+    let url = format!("{addr}/{repo}/_tag/{name}");
 
     let res = cl.head(&url).send().await.unwrap();
     assert_eq!(
@@ -31,9 +35,13 @@ pub async fn create(
         res.text().await.unwrap()
     );
 
+    let body = serde_json::to_vec(&tag).unwrap();
+    let body_digest = Algorithms::default().read(&body[..]).await.unwrap();
+
     let res = cl
         .put(&url)
         .header(CONTENT_TYPE, TreeEntry::TYPE)
+        .header("content-digest", &body_digest.to_string())
         .body(serde_json::to_vec(&tag).unwrap())
         .send()
         .await
@@ -52,7 +60,15 @@ pub async fn create(
         "{}",
         res.text().await.unwrap()
     );
-    // TODO: Validate headers
+
+    let content_length = res.content_length().unwrap();
+    assert_eq!(content_length, body.len() as u64);
+
+    let content_type: Mime = parse_header(res.headers(), CONTENT_TYPE.as_str());
+    assert_eq!(content_type, TreeEntry::TYPE);
+
+    let content_digest: ContentDigest = parse_header(res.headers(), "content-digest");
+    assert_eq!(body_digest, content_digest);
 
     let res = cl.get(&url).send().await.unwrap();
     assert_eq!(
@@ -61,12 +77,22 @@ pub async fn create(
         "{}",
         res.text().await.unwrap()
     );
+
+    let content_length = res.content_length().unwrap();
+    assert_eq!(content_length, body.len() as u64);
+
+    let content_type: Mime = parse_header(res.headers(), CONTENT_TYPE.as_str());
+    assert_eq!(content_type, TreeEntry::TYPE);
+
+    let content_digest: ContentDigest = parse_header(res.headers(), "content-digest");
+    assert_eq!(body_digest, content_digest);
+
     assert_eq!(res.json::<TagEntry>().await.unwrap(), tag);
-    // TODO: Validate headers
 
     let res = cl
         .put(&url)
         .header(CONTENT_TYPE, TreeEntry::TYPE)
+        .header("content-digest", &body_digest.to_string())
         .body(serde_json::to_vec(&tag).unwrap())
         .send()
         .await
@@ -81,11 +107,7 @@ pub async fn create(
 }
 
 pub async fn list(cl: &reqwest::Client, addr: &str, repo: &RepositoryName) -> Vec<TagName> {
-    let res = cl
-        .get(format!("{}/{}/_tag", addr, repo))
-        .send()
-        .await
-        .unwrap();
+    let res = cl.get(format!("{addr}/{repo}/_tag")).send().await.unwrap();
     assert_eq!(
         res.status(),
         StatusCode::OK,
