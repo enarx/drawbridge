b42674aefa3e71094ed4acf0206789a4daf3351b
Roman Volosatovs <roman@profian.com>
2022-06-09 20:16:42 +0200

    feat: compute tag query response `Content-Digest`
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/app/src/tags/query.rs
+++ crates/app/src/tags/query.rs
@@ -5,22 +5,50 @@ use super::super::Store;
 
 use std::sync::Arc;
 
-use drawbridge_type::RepositoryName;
+use drawbridge_type::digest::Algorithms;
+use drawbridge_type::{Meta, RepositoryContext};
 
-use axum::response::IntoResponse;
-use axum::{Extension, Json};
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::Extension;
+use mime::APPLICATION_JSON;
 
 pub async fn query(
     Extension(store): Extension<Arc<Store>>,
-    Extension(repo): Extension<RepositoryName>,
+    repo: RepositoryContext,
 ) -> impl IntoResponse {
-    store
-        .repository(&repo)
-        .tags()
-        .await
-        .map_err(|e| {
-            eprintln!("Failed to GET tags on repository `{}`: {:?}", repo, e);
-            e
-        })
-        .map(Json)
+    {
+        let tags = store
+            .repository(&repo)
+            .tags()
+            .await
+            .map_err(IntoResponse::into_response)?;
+        // TODO: Optimize hash computation
+        let buf = serde_json::to_vec(&tags).map_err(|e| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to encode tags as JSON: {e}"),
+            )
+                .into_response()
+        })?;
+        let hash = Algorithms::default().read_sync(&buf[..]).map_err(|e| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to compute tag digest: {e}"),
+            )
+                .into_response()
+        })?;
+        Ok::<_, Response>((
+            Meta {
+                hash,
+                size: buf.len() as _,
+                mime: APPLICATION_JSON,
+            },
+            buf,
+        ))
+    }
+    .map_err(|e| {
+        eprintln!("Failed to GET tags on `{}`: {:?}", repo, e);
+        e
+    })
 }
