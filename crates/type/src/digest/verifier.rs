3ba18d7c1f41577c8851229105235bc29a77cdca
Roman Volosatovs <roman@profian.com>
2022-06-09 20:09:01 +0200

    feat: implement `std::io::Read` for `digest::Verifier`
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/type/src/digest/verifier.rs
+++ crates/type/src/digest/verifier.rs
@@ -74,6 +74,20 @@ where
     }
 }
 
+impl<T: io::Read, H> io::Read for Verifier<T, H>
+where
+    H: AsRef<[u8]> + From<Vec<u8>>,
+{
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        match self.reader.read(buf)? {
+            0 if self.reader.digests() != self.hashes => {
+                Err(Error::new(ErrorKind::InvalidData, "hash mismatch"))
+            }
+            n => Ok(n),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use futures::io::{copy, sink};
@@ -82,16 +96,42 @@ mod tests {
 
     #[tokio::test]
     async fn read_success() {
-        const HASH: &str = "sha-256=:LCa0a2j/xo/5m0U8HTBBNBNCLXBkg7+g+YpeiGJm564=:";
-        let mut reader = HASH.parse::<ContentDigest>().unwrap().verifier(&b"foo"[..]);
-        copy(&mut reader, &mut sink()).await.unwrap();
+        let rdr = &b"foo"[..];
+        let content_digest = "sha-224=:CAj2TmDViXn8tnbJbsk4Jw3qQkRa7vzTpOb42w==:,sha-256=:LCa0a2j/xo/5m0U8HTBBNBNCLXBkg7+g+YpeiGJm564=:,sha-384=:mMEf/f3VQGdrGhN8saIrKnA1DJpEFx1rEYDGvly7LuP3nVMsih3Z7y6OCOdSo7q7:,sha-512=:9/u6bgY2+JDlb7vzKD5STG+jIErimDgtYkdB0NxmODJuKCxBvl5CVNiCB3LFUYosWowMf37aGVlKfrU5RT4e1w==:"
+                .parse::<ContentDigest>()
+                .unwrap();
+
+        assert_eq!(
+            copy(&mut content_digest.clone().verifier(rdr), &mut sink())
+                .await
+                .unwrap(),
+            "foo".len() as u64,
+        );
+        assert_eq!(
+            std::io::copy(&mut content_digest.verifier(rdr), &mut std::io::sink()).unwrap(),
+            "foo".len() as u64,
+        );
     }
 
     #[tokio::test]
     async fn read_failure() {
-        const HASH: &str = "sha-256=:LCa0a2j/xo/5m0U8HTBBNBNCLXBkg7+g+YpeiGJm564=:";
-        let mut reader = HASH.parse::<ContentDigest>().unwrap().verifier(&b"bar"[..]);
-        let err = copy(&mut reader, &mut sink()).await.unwrap_err();
-        assert_eq!(err.kind(), ErrorKind::InvalidData);
+        let rdr = &b"bar"[..];
+        let content_digest = "sha-224=:CAj2TmDViXn8tnbJbsk4Jw3qQkRa7vzTpOb42w==:,sha-256=:LCa0a2j/xo/5m0U8HTBBNBNCLXBkg7+g+YpeiGJm564=:,sha-384=:mMEf/f3VQGdrGhN8saIrKnA1DJpEFx1rEYDGvly7LuP3nVMsih3Z7y6OCOdSo7q7:,sha-512=:9/u6bgY2+JDlb7vzKD5STG+jIErimDgtYkdB0NxmODJuKCxBvl5CVNiCB3LFUYosWowMf37aGVlKfrU5RT4e1w==:"
+                .parse::<ContentDigest>()
+                .unwrap();
+
+        assert_eq!(
+            copy(&mut content_digest.clone().verifier(rdr), &mut sink())
+                .await
+                .unwrap_err()
+                .kind(),
+            ErrorKind::InvalidData,
+        );
+        assert_eq!(
+            std::io::copy(&mut content_digest.verifier(rdr), &mut std::io::sink())
+                .unwrap_err()
+                .kind(),
+            ErrorKind::InvalidData,
+        );
     }
 }
