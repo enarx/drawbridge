1216c849efdc3fe5c6c717c1e3f2d4991eb4fad2
Roman Volosatovs <roman@profian.com>
2022-06-16 18:51:17 +0200

    feat(client): add client-side TLS support
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -22,9 +22,18 @@ pub use anyhow::{Context, Result};
 pub use mime;
 pub use url::Url;
 
+use std::sync::Arc;
+
 use drawbridge_type::{RepositoryContext, TagContext, TreeContext, UserContext};
 
-#[derive(Debug)]
+use rustls::cipher_suite::{
+    TLS13_AES_128_GCM_SHA256, TLS13_AES_256_GCM_SHA384, TLS13_CHACHA20_POLY1305_SHA256,
+};
+use rustls::kx_group::{SECP256R1, SECP384R1, X25519};
+use rustls::version::TLS13;
+use rustls::{Certificate, OwnedTrustAnchor, PrivateKey, RootCertStore};
+
+#[derive(Clone, Debug)]
 pub struct Client {
     inner: ureq::Agent,
     root: Url,
@@ -33,6 +42,10 @@ pub struct Client {
 impl Client {
     pub fn builder(url: Url) -> ClientBuilder {
         ClientBuilder::new(url)
+    }
+
+    pub fn new(url: Url) -> Result<Self> {
+        Self::builder(url).build()
     }
 
     fn url(&self, path: &str) -> Result<Url> {
@@ -59,25 +72,69 @@ impl Client {
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct ClientBuilder {
-    inner: ureq::AgentBuilder,
     url: Url,
+    credentials: Option<(Vec<Certificate>, PrivateKey)>,
+    roots: Option<RootCertStore>,
 }
 
 impl ClientBuilder {
     pub fn new(url: Url) -> Self {
         Self {
-            inner: ureq::AgentBuilder::new(),
             url,
+            credentials: None,
+            roots: None,
         }
     }
 
-    // TODO: Add configuration
-
-    pub fn build(self) -> Client {
-        Client {
-            inner: self.inner.build(),
-            root: self.url,
+    pub fn credentials(self, cert: Vec<Certificate>, key: PrivateKey) -> Self {
+        Self {
+            credentials: Some((cert, key)),
+            ..self
         }
+    }
+
+    pub fn roots(self, roots: RootCertStore) -> Self {
+        Self {
+            roots: Some(roots),
+            ..self
+        }
+    }
+
+    pub fn build(self) -> Result<Client> {
+        let tls = rustls::ClientConfig::builder()
+            .with_cipher_suites(&[
+                TLS13_AES_256_GCM_SHA384,
+                TLS13_AES_128_GCM_SHA256,
+                TLS13_CHACHA20_POLY1305_SHA256,
+            ])
+            .with_kx_groups(&[&X25519, &SECP384R1, &SECP256R1])
+            .with_protocol_versions(&[&TLS13])?
+            .with_root_certificates(if let Some(roots) = self.roots {
+                roots
+            } else {
+                let mut root_store = RootCertStore::empty();
+                root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
+                    |ta| {
+                        OwnedTrustAnchor::from_subject_spki_name_constraints(
+                            ta.subject,
+                            ta.spki,
+                            ta.name_constraints,
+                        )
+                    },
+                ));
+                root_store
+            });
+        let tls = if let Some((cert, key)) = self.credentials {
+            tls.with_single_cert(cert, key)?
+        } else {
+            tls.with_no_client_auth()
+        };
+
+        Ok(Client {
+            inner: ureq::AgentBuilder::new().tls_config(Arc::new(tls)).build(),
+            root: self.url,
+        })
     }
 }
