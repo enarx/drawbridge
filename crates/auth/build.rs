25a832e6e1d0b59260502547e0134b645021e687
Roman Volosatovs <roman@profian.com>
2022-06-22 20:11:54 +0200

    chore!: remove `auth` crate
    The crate has been moved to https://github.com/profianinc/benefice in https://github.com/profianinc/benefice/pull/10
    
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/auth/build.rs
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
-// SPDX-License-Identifier: AGPL-3.0-only
-
-fn main() {
-    if option_env!("GITHUB_TOKEN").is_some() {
-        println!("cargo:rustc-cfg=has_github_token");
-    }
-}
--- crates/auth/tests/providers/mod.rs
+++
@@ -1,4 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
-// SPDX-License-Identifier: AGPL-3.0-only
-
-mod github;
