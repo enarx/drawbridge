5cbb2f345be0c786ae8e11642bff7b9b4a759380
Roman Volosatovs <roman@profian.com>
2022-06-22 18:30:52 +0200

    build(nix): add release build outputs
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
   description = "Profian Drawbridge";
 
-  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.fenix.url = github:nix-community/fenix;
+  inputs.cargo2nix.inputs.flake-compat.follows = "flake-compat";
+  inputs.cargo2nix.inputs.flake-utils.follows = "flake-utils";
+  inputs.cargo2nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.cargo2nix.inputs.rust-overlay.follows = "rust-overlay";
+  inputs.cargo2nix.url = github:cargo2nix/cargo2nix;
   inputs.flake-compat.flake = false;
   inputs.flake-compat.url = github:edolstra/flake-compat;
   inputs.flake-utils.url = github:numtide/flake-utils;
   inputs.nixpkgs.url = github:NixOS/nixpkgs/master;
+  inputs.rust-overlay.inputs.flake-utils.follows = "flake-utils";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.rust-overlay.url = github:oxalica/rust-overlay;
 
   outputs =
     { self
 
-    , fenix
+    , cargo2nix
     , flake-utils
     , nixpkgs
     , ...
@@ -19,18 +25,67 @@
 
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ cargo2nix.overlays.default ];
+      };
 
-      rust = fenix.packages."${system}".fromToolchainFile {
-        file = "${self}/rust-toolchain.toml";
+      cargo2nixBin = cargo2nix.packages.${system}.cargo2nix;
+      devRust = pkgs.rust-bin.fromRustupToolchainFile "${self}/rust-toolchain.toml";
+
+      cargo.toml = builtins.fromTOML (builtins.readFile "${self}/Cargo.toml");
+
+      mkBin = pkgs: (
+        (pkgs.rustBuilder.makePackageSet {
+          packageFun = import "${self}/Cargo.nix";
+          rustVersion = "1.61.0";
+          workspaceSrc = pkgs.nix-gitignore.gitignoreRecursiveSource [
+            "*.nix"
+            "*.yml"
+            "/.github"
+            "flake.lock"
+            "LICENSE"
+            "rust-toolchain.toml"
+          ]
+            self;
+        }).workspace."${cargo.toml.package.name}" { }
+      ).bin;
+
+      nativeBin = mkBin pkgs;
+      x86_64LinuxMuslBin = mkBin (import nixpkgs {
+        inherit system;
+        crossSystem = {
+          config = "x86_64-unknown-linux-musl";
+        };
+        overlays = [ cargo2nix.overlays.default ];
+      });
+
+      buildImage = bin: pkgs.dockerTools.buildImage {
+        inherit (cargo.toml.package) name;
+        tag = cargo.toml.package.version;
+        contents = [
+          bin
+        ];
+        config.Cmd = [ cargo.toml.package.name ];
+        config.Env = [ "PATH=${bin}/bin" ];
       };
     in
     {
+      defaultPackage = nativeBin;
+
+      packages = {
+        "${cargo.toml.package.name}" = nativeBin;
+        "${cargo.toml.package.name}-x86_64-unknown-linux-musl" = x86_64LinuxMuslBin;
+        "${cargo.toml.package.name}-x86_64-unknown-linux-musl-oci" = buildImage x86_64LinuxMuslBin;
+      };
+
       devShell = pkgs.mkShell {
         buildInputs = [
           pkgs.openssl
 
-          rust
+          cargo2nixBin
+
+          devRust
         ];
       };
     }
