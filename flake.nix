9a63f3c00e6e1402d30aa40209c46217e86331e5
Roman Volosatovs <roman@profian.com>
2022-07-04 15:10:29 +0200

    build(nix): add debug builds
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- flake.nix
+++ flake.nix
@@ -27,41 +27,48 @@
           inherit system;
           overlays = [cargo2nix.overlays.default];
         };
+        pkgsX86_64LinuxMusl = import nixpkgs {
+          inherit system;
+          crossSystem = {
+            config = "x86_64-unknown-linux-musl";
+          };
+          overlays = [cargo2nix.overlays.default];
+        };
 
         cargo2nixBin = cargo2nix.packages.${system}.cargo2nix;
         devRust = pkgs.rust-bin.fromRustupToolchainFile "${self}/rust-toolchain.toml";
 
         cargo.toml = builtins.fromTOML (builtins.readFile "${self}/Cargo.toml");
 
-        mkBin = pkgs:
-          (
-            (pkgs.rustBuilder.makePackageSet {
-              packageFun = import "${self}/Cargo.nix";
-              rustVersion = "1.61.0";
-              workspaceSrc =
-                pkgs.nix-gitignore.gitignoreRecursiveSource [
-                  "*.nix"
-                  "*.yml"
-                  "/.github"
-                  "flake.lock"
-                  "LICENSE"
-                  "rust-toolchain.toml"
-                ]
-                self;
-            })
+        mkBin = args: pkgs:
+          ((pkgs.rustBuilder.makePackageSet ({
+                packageFun = import "${self}/Cargo.nix";
+                rustVersion = "1.61.0";
+                workspaceSrc =
+                  pkgs.nix-gitignore.gitignoreRecursiveSource [
+                    "*.nix"
+                    "*.yml"
+                    "/.github"
+                    "flake.lock"
+                    "LICENSE"
+                    "rust-toolchain.toml"
+                  ]
+                  self;
+              }
+              // args))
             .workspace
-            ."${cargo.toml.package.name}" {}
-          )
+            ."${cargo.toml.package.name}" {})
           .bin;
 
-        nativeBin = mkBin pkgs;
-        x86_64LinuxMuslBin = mkBin (import nixpkgs {
-          inherit system;
-          crossSystem = {
-            config = "x86_64-unknown-linux-musl";
-          };
-          overlays = [cargo2nix.overlays.default];
-        });
+        mkReleaseBin = mkBin {};
+
+        nativeBin = mkReleaseBin pkgs;
+        x86_64LinuxMuslBin = mkReleaseBin pkgsX86_64LinuxMusl;
+
+        mkDebugBin = mkBin {release = false;};
+
+        nativeDebugBin = mkDebugBin pkgs;
+        x86_64LinuxMuslDebugBin = mkDebugBin pkgsX86_64LinuxMusl;
 
         buildImage = bin:
           pkgs.dockerTools.buildImage {
@@ -80,6 +87,10 @@
           "${cargo.toml.package.name}" = nativeBin;
           "${cargo.toml.package.name}-x86_64-unknown-linux-musl" = x86_64LinuxMuslBin;
           "${cargo.toml.package.name}-x86_64-unknown-linux-musl-oci" = buildImage x86_64LinuxMuslBin;
+
+          "${cargo.toml.package.name}-debug" = nativeDebugBin;
+          "${cargo.toml.package.name}-debug-x86_64-unknown-linux-musl" = x86_64LinuxMuslDebugBin;
+          "${cargo.toml.package.name}-debug-x86_64-unknown-linux-musl-oci" = buildImage x86_64LinuxMuslDebugBin;
         };
         packages.default = nativeBin;
 
