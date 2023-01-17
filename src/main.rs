enarx/drawbridge

commit d445a946b7a73e4d00a70b186007bef774708901

feat: migrate to `async-std`
Signed-off-by: Roman Volosatovs <roman@profian.com>

src/main.rs
@@ -5,26 +5,38 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::path::{Path, PathBuf};
-use std::sync::Arc;
 
-use drawbridge_app::{self as app, TLSConfig};
+use drawbridge_app::{App, TlsConfig};
 
-use axum_server::bind_rustls;
-use axum_server::tls_rustls::RustlsConfig;
+use anyhow::Context as _;
+use async_std::net::TcpListener;
 use clap::Parser;
+use futures::StreamExt;
+use log::{debug, error};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
+    /// Address to bind to.
+    #[clap(long, default_value_t = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 8080))]
+    addr: SocketAddr,
+
+    /// Path to the Drawbridge store.
     #[clap(long)]
     store: PathBuf,
 
+    /// Path to PEM-encoded server certificate.
     #[clap(long)]
     cert: PathBuf,
 
+    /// Path to PEM-encoded server certificate key.
     #[clap(long)]
     key: PathBuf,
 
+    /// Path to PEM-encoded trusted CA certificate.
+    ///
+    /// Clients that present a valid certificate signed by this CA
+    /// are granted read-only access to all repositories in the store.
     #[clap(long)]
     ca: PathBuf,
 }
@@ -33,8 +45,8 @@ fn open_buffered(p: impl AsRef<Path>) -> io::Result<impl BufRead> {
     File::open(p).map(BufReader::new)
 }
 
-#[tokio::main]
-async fn main() {
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
     let Args {
@@ -42,25 +54,40 @@ async fn main() {
         cert,
         key,
         ca,
+        addr,
     } = Args::parse();
 
-    // TODO: Proper error handling
+    let cert = open_buffered(cert).context("Failed to open server certificate file")?;
+    let key = open_buffered(key).context("Failed to open server key file")?;
+    let ca = open_buffered(ca).context("Failed to open CA certificate file")?;
+    let tls = TlsConfig::read(cert, key, ca).context("Failed to construct server TLS config")?;
 
-    let cert = open_buffered(cert).expect("Failed to open server certificate file");
-    let key = open_buffered(key).expect("Failed to open server key file");
-    let ca = open_buffered(ca).expect("Failed to open CA certificate file");
-
-    let tls = TLSConfig::read(cert, key, ca)
-        .map(Into::into)
-        .map(Arc::new)
-        .map(RustlsConfig::from_config)
-        .expect("Failed to construct server TLS config");
-
-    bind_rustls(
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 8080),
-        tls,
-    )
-    .serve(app::Builder::new(store).build().unwrap())
-    .await
-    .unwrap();
+    let app = App::builder(store, tls)
+        .build()
+        .await
+        .context("Failed to build app")?;
+    TcpListener::bind(addr)
+        .await
+        .with_context(|| format!("Failed to bind to {}", addr))?
+        .incoming()
+        .for_each_concurrent(Some(1), |stream| async {
+            if let Err(e) = async {
+                let stream = stream.context("failed to initialize connection")?;
+                debug!(
+                    target: "main",
+                    "received TCP connection from {}",
+                    stream
+                        .peer_addr()
+                        .map(|peer| peer.to_string())
+                        .unwrap_or_else(|_| "unknown address".into())
+                );
+                app.handle(stream).await
+            }
+            .await
+            {
+                error!(target: "main", "failed to handle request: {e}");
+            }
+        })
+        .await;
+    Ok(())
 }

Cargo.toml
@@ -19,17 +19,21 @@ members = [
 drawbridge-app = { path = "./crates/app" }
 
 # External dependencies
-axum-server = { version = "0.4.0", default-features = false, features = ["tls-rustls"] }
+anyhow = { version = "1.0.57", default-features = false, features = ["std"] }
+async-std = { version = "1.11.0", default-features = false, features = ["attributes"] }
 clap = { version = "3.2.3", default-features = false, features = ["derive", "std"] }
 env_logger = { version = "0.9.0", default-features = false, features = ["atty", "termcolor"] }
-tokio = { version = "1.17.0", default-features = false, features = ["macros", "rt-multi-thread"] }
+futures = { version = "0.3.21", default-features = false, features = ["io-compat"] }
+log = { version = "0.4.17", default-features = false }
 
 [dev-dependencies]
 # Internal dependencies
 drawbridge-client = { path = "./crates/client" }
 
 # External dependencies
+async-std = { version = "1.11.0", default-features = false, features = ["attributes", "default"] }
 futures = { version = "0.3.21", default-features = false }
-hyper = { version = "0.14.18", default-features = false, features = ["http1", "server", "tcp"] }
+rustls = { version = "0.20.6", default-features = false }
+rustls-pemfile = { version = "1.0.0", default-features = false }
 serde_json = { version = "1.0.79", default-features = false, features = ["std"] }
 tempfile = { version = "3.3.0", default-features = false }

crates/app/Cargo.toml
@@ -11,20 +11,25 @@ drawbridge-type = { path = "../type", features = ["server"] }
 
 # External dependencies
 anyhow = { version = "1.0.57", default-features = false, features = ["std"] }
+async-std = { version = "1.11.0", default-features = false }
 axum = { version = "0.5.4", default-features = false, features = ["json"] }
 camino = { version = "1.0.9", default-features = false }
 cap-async-std = { version = "0.24.4", default-features = true, features = ["fs_utf8"] }
 futures = { version = "0.3.21", default-features = false, features = ["async-await"] }
+futures-rustls = { version = "0.22.1", default-features = false }
+hyper = { version = "0.14.18", default-features = false, features = ["http1", "server"] }
 log = { version = "0.4.17", default-features = false }
 mime = { version = "0.3.16", default-features = false }
 rustls = { version = "0.20.6", default-features = false }
 rustls-pemfile = { version = "1.0.0", default-features = false }
 serde = { version = "1.0.136", default-features = false }
 serde_json = { version = "1.0.79", default-features = false, features = ["std"] }
+tokio-util = { version = "0.7.3", default-features = false, features = ["compat"] }
 tower = { version = "0.4.12", default-features = false }
 
 [dev-dependencies]
+async-std = { version = "1.11.0", default-features = false, features = ["attributes"] }
 bytes = { version = "1.1.0", default-features = false }
+env_logger = { version = "0.9.0", default-features = false, features = ["atty", "termcolor"] }
 reqwest = { version = "0.11.10", default-features = false, features = ["json"] }
 tempfile = { version = "3.3.0", default-features = false }
-tokio = { version = "1.17.0", default-features = false, features = ["macros", "rt"] }

crates/app/src/auth/mod.rs
@@ -1,0 +1,6 @@
+// SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
+// SPDX-License-Identifier: AGPL-3.0-only
+
+mod tls;
+
+pub use tls::{Config as TlsConfig, TrustedCertificate};

crates/app/src/auth/tls.rs
@@ -9,10 +9,14 @@ use rustls::cipher_suite::{
     TLS13_AES_128_GCM_SHA256, TLS13_AES_256_GCM_SHA384, TLS13_CHACHA20_POLY1305_SHA256,
 };
 use rustls::kx_group::{SECP256R1, SECP384R1, X25519};
-use rustls::server::AllowAnyAuthenticatedClient;
+use rustls::server::AllowAnyAnonymousOrAuthenticatedClient;
 use rustls::version::TLS13;
 use rustls::{Certificate, PrivateKey, RootCertStore, ServerConfig};
 use rustls_pemfile::Item::{ECKey, PKCS8Key, RSAKey, X509Certificate};
+
+#[derive(Clone, Copy, Debug)]
+#[repr(transparent)]
+pub struct TrustedCertificate;
 
 #[repr(transparent)]
 pub struct Config(ServerConfig);
@@ -54,7 +58,7 @@ impl Config {
                 .context("failed to read server certificate key")?;
             let key = items
                 .pop()
-                .ok_or(anyhow!("server certificate key missing"))
+                .ok_or_else(|| anyhow!("server certificate key missing"))
                 .and_then(|item| match item {
                     RSAKey(buf) | PKCS8Key(buf) | ECKey(buf) => Ok(PrivateKey(buf)),
                     _ => bail!("unsupported key type"),
@@ -72,10 +76,11 @@ impl Config {
                 .into_iter()
                 .try_for_each(|ref cert| roots.add(cert))
                 .context("failed to construct root certificate store")?;
-            AllowAnyAuthenticatedClient::new(roots)
+            // TODO: Allow client certificates signed by unknown CAs.
+            AllowAnyAnonymousOrAuthenticatedClient::new(roots)
         };
 
-        // TODO: load policy from config.
+        // TODO: Load policy from config.
         ServerConfig::builder()
             .with_cipher_suites(&[
                 TLS13_AES_256_GCM_SHA384,

crates/app/src/builder.rs
@@ -1,37 +1,43 @@
 // SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 
-use super::{handle, Store};
-
-use std::fs::File;
-use std::path::Path;
-use std::sync::Arc;
+use super::{handle, App, Store, TlsConfig};
 
 use anyhow::{anyhow, Context};
+use async_std::fs::File;
+use async_std::sync::Arc;
 use axum::handler::Handler;
-use axum::routing::IntoMakeService;
 use axum::{Extension, Router};
+use cap_async_std::path::Path;
+use futures::lock::Mutex;
+use futures_rustls::TlsAcceptor;
 
 pub struct Builder<S> {
     store: S,
+    tls: TlsConfig,
 }
 
 impl<S: AsRef<Path>> Builder<S> {
     /// Constructs a new [Builder].
-    pub fn new(store: S) -> Self {
-        Self { store }
+    pub fn new(store: S, tls: TlsConfig) -> Self {
+        Self { store, tls }
     }
 
     /// Builds the application and returns Drawbridge instance as a [tower::MakeService].
-    pub fn build(self) -> anyhow::Result<IntoMakeService<Router>> {
+    pub async fn build(self) -> anyhow::Result<App> {
         let path = self.store.as_ref();
-        let store = File::open(path).map(Store::from).context(anyhow!(
+        let store = File::open(path).await.map(Store::from).context(anyhow!(
             "failed to open store at `{}`",
             path.to_string_lossy()
         ))?;
-        Ok(Router::new()
-            .fallback(handle.into_service())
-            .layer(Extension(Arc::new(store)))
-            .into_make_service())
+        Ok(App {
+            make_service: Mutex::new(
+                Router::new()
+                    .fallback(handle.into_service())
+                    .layer(Extension(Arc::new(store)))
+                    .into_make_service(),
+            ),
+            tls: TlsAcceptor::from(Arc::new(self.tls.into())),
+        })
     }
 }

crates/app/src/lib.rs
@@ -6,18 +6,74 @@
 
 mod builder;
 mod handle;
-mod repos;
-mod store;
-mod tags;
-mod tls;
-mod trees;
-mod users;
 
+pub mod auth;
+pub mod repos;
+pub mod store;
+pub mod tags;
+pub mod trees;
+pub mod users;
+
+pub use auth::*;
 pub use builder::*;
 pub(crate) use handle::*;
-pub use repos::*;
 pub(crate) use store::*;
-pub use tags::*;
-pub use tls::Config as TLSConfig;
-pub use trees::*;
-pub use users::*;
+
+use anyhow::Context as _;
+use axum::extract::Extension;
+use axum::routing::IntoMakeService;
+use axum::Router;
+use cap_async_std::path::Path;
+use futures::lock::Mutex;
+use futures::{AsyncRead, AsyncWrite};
+use futures_rustls::TlsAcceptor;
+use hyper::server::conn::Http;
+use log::trace;
+use tokio_util::compat::FuturesAsyncReadCompatExt;
+use tower::MakeService;
+
+pub struct App {
+    make_service: Mutex<IntoMakeService<Router>>,
+    tls: TlsAcceptor,
+}
+
+impl App {
+    pub fn builder<S: AsRef<Path>>(store: S, tls: TlsConfig) -> Builder<S> {
+        Builder::new(store, tls)
+    }
+
+    pub async fn new(store: impl AsRef<Path>, tls: TlsConfig) -> anyhow::Result<Self> {
+        Self::builder(store, tls).build().await
+    }
+
+    pub async fn handle(
+        &self,
+        stream: impl 'static + Unpin + AsyncRead + AsyncWrite,
+    ) -> anyhow::Result<()> {
+        trace!(target: "app::App::handle", "begin TLS handshake");
+        let stream = self
+            .tls
+            .accept(stream)
+            .await
+            .context("failed to accept TLS connection")?;
+        trace!(target: "app::App::handle", "completed TLS handshake");
+
+        let mut svc = self
+            .make_service
+            .lock()
+            .await
+            .make_service(())
+            .await
+            .context("failed to create app service")?;
+        let (_, conn) = stream.get_ref();
+        if conn.peer_certificates().is_some() {
+            svc = svc.layer(Extension(TrustedCertificate));
+            trace!(target: "app::App::handle", "add TrustedCertificate to extensions");
+        }
+        trace!(target: "app::App::handle", "begin HTTP request serving");
+        Http::new()
+            .serve_connection(stream.compat(), svc)
+            .await
+            .context("failed to handle request")
+    }
+}

crates/app/src/repos/get.rs
@@ -3,10 +3,9 @@
 
 use super::super::Store;
 
-use std::sync::Arc;
-
 use drawbridge_type::RepositoryContext;
 
+use async_std::sync::Arc;
 use axum::response::IntoResponse;
 use axum::Extension;
 use log::warn;

commit a62093e99953a2618fd29ace65ec5897a8621eee

fix: remove limit of 1 active connection
This was put in place during the move to async-std, and limits the number of
active connections to 1.

Fixes: d445a946
Signed-off-by: Patrick Uiterwijk <patrick@profian.com>

src/main.rs
@@ -131,7 +131,7 @@ async fn main() -> anyhow::Result<()> {
         .await
         .with_context(|| format!("Failed to bind to {}", addr))?
         .incoming()
-        .for_each_concurrent(Some(1), |stream| async {
+        .for_each_concurrent(None, |stream| async {
             if let Err(e) = async {
                 let stream = stream.context("failed to initialize connection")?;
                 debug!(