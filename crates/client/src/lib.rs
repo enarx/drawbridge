7334ee1a898b49a509b53dab1e923ce7f4360601
Richard Zak <richard.j.zak@gmail.com>
2023-10-03 20:08:10 -0400

    chore(deps): update dependencies
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -208,7 +208,7 @@ impl<S: Scope> ClientBuilder<S> {
                 roots
             } else {
                 let mut root_store = RootCertStore::empty();
-                root_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+                root_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
                     OwnedTrustAnchor::from_subject_spki_name_constraints(
                         ta.subject,
                         ta.spki,
